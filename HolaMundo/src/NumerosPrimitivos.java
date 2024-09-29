0c631755e04b87fc31544af7f3fcb2b8a56cba13
FaurregobFreddy <faurregob@gmail.com>
2024-09-29 17:59:09 -0500

    Actualizar Clase

---
+++ HolaMundo/src/NumerosPrimitivos.java
@@ -0,0 +1,10 @@
+public class NumerosPrimitivos {
+    public static void main(String[] args) {
+        byte numeroByte = 127;
+        System.out.println("numeroByte = " + numeroByte);
+        System.out.println("Tipo byte corresponde en byte a: "+Byte.BYTES);
+        System.out.println("Tipo byte corresponde en byte a: "+Byte.SIZE);
+        System.out.println("Valor Maximo de un byte: "+Byte.MAX_VALUE);
+        System.out.println("Valor minimo de un byte: "+Byte.MIN_VALUE);
+    }
+}
